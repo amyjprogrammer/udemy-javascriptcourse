b54097625b592dc9238811ca04792d56c55d30af
amyjprogrammer <amyjprogrammer@gmail.com>
2022-01-09 23:55:02 -0500

    Value and Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,46 @@
+/* First lecture- variables
 let js = "amazing";
 console.log(40 + 8 + 23 -10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.1415; //const (all upper case)
+
+//exercises
+let country = "USA";
+let continent = "North America";
+let population = 325;
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+// true; //bool value
+// console.log(true);
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = "Yes!";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year); //both are undefined
+
+year = 1995; //assigning a new value
+
+//exercises
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof language);
