532d8622d98a545b5194174ac3640415f98a2b29
amyjprogrammer <amyjprogrammer@gmail.com>
2022-01-10 00:59:19 -0500

    Operators and Challenge

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -21,6 +21,7 @@ console.log(continent);
 console.log(population);
 */
 
+/* Second
 // true; //bool value
 // console.log(true);
 let javascriptIsFun = true;
@@ -44,3 +45,63 @@ let isIsland = false;
 let language;
 console.log(typeof isIsland);
 console.log(typeof language);
+*/
+
+/*  Third
+let age;
+age = 33;
+
+//const needs an intial value
+const birthYear = 1977; //can't be changed
+//birthYear = 1995;//can't reassign--causes issue
+*/
+
+/* Fourth
+const now = 2037;
+const ageAmy = now - 1977;
+const ageSara = now - 2015;
+console.log(ageAmy, ageSara);
+console.log(ageAmy / 2, ageAmy * 4, 2 **3);
+
+const firstName = "Jane";
+const lastName = "Doe";
+console.log(firstName + " " + lastName);
+
+//assignment operators
+let x = 10 + 5; //15
+x += 10; //x = x + 10 is 25
+x *= 4; //x = x * 4 is 100
+x++ //x = x + 1 is 101
+x--;
+x--; //now 99
+console.log(x);
+
+//comparison operators
+console.log( ageAmy > ageSara);
+*/
+
+/*
+//Operator Precedence
+const now = 2037;
+const ageAmy = now - 1977;
+const ageSara = now - 2015;
+
+console.log(now - 1977 > now - 2015);
+console.log(25-10-5);
+
+let x, y;
+x = y = 25-10-5;
+console.log(x, y);//both are 10
+
+//Coding Challenge
+let markWeight = 95;
+let markHeight = 1.88;
+let johnWeight = 85;
+let johnHeight = 1.76;
+
+markBMI = markWeight / markHeight ** 2;
+johnBMI = johnWeight / johnHeight ** 2;
+
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+*/
