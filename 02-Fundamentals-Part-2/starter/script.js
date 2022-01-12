347e44fe5b2e7097e3b287621359250b96356fae
amyjprogrammer <amyjprogrammer@gmail.com>
2022-01-12 16:41:52 -0500

    functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,54 @@
+'use strict';//activated strict mode
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true; //with strict mode the console have feedback that hasDriverLicense is not defined..it is misspelled
+if(hasDriversLicense) console.log("I can drive!");
+
+const interface = 'Audio';//this shows unexpected strict mode reserved word
+*/
+
+/*
+//functions
+
+function logger(){
+    console.log('My name is Amy');
+}
+
+logger(); //invoking the function, running the function or calling 
+logger();
+
+//apples and oranges are the parameters
+function fruitProcessor(apples, oranges){
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);//3 and 4 are the arguments
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
+/*
+//function declaration
+function calcAge1(birthdate){
+    return 2022 - birthdate;
+}
+
+const amyAge = calcAge1(1977);
+
+//Function expression
+const calcAge2 = function (birthYear){
+    return 2022 - birthYear;
+}
+
+const age2 = calcAge2(1977);
+
+console.log(amyAge, age2);
+*/
+
+//arrow functions
