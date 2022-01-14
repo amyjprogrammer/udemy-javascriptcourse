77eb9ee08a06e0ab26846ddfce051c1d577dea59
amyjprogrammer <amyjprogrammer@gmail.com>
2022-01-13 23:24:10 -0500

    Arrays and Challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -133,3 +133,82 @@ checkWinner(calcAverage(85, 54, 41), calcAverage(23, 34, 27));
 checkWinner(calcAverage(25, 34, 22), calcAverage(85, 54, 41));
 */
 
+/*
+//Arrays
+const friends = ['David', 'Bob', 'Barb'];
+console.log(friends);
+
+// const years = new Array(1991, 1984, 2013, 2016);
+
+console.log(friends[0]);
+console.log(friends[2]);
+console.log(friends.length);
+
+console.log(friends[friends.length-1]);
+
+friends[2] = 'Jesse';
+console.log(friends[2]);
+
+const firstName = "Amy";
+const amy = [firstName, 'Jones', 2022 - 1977, 'Front End Engineer', friends];
+console.log(amy);
+console.log(amy.length);
+
+const calcAge = function(birthYear){
+    return 2022 - birthYear;
+}
+
+const years = [1990, 1977, 2001, 1984, 2010];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length-1]);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length-1])];
+console.log(ages);
+*/
+
+/*
+const friends = ['David', 'Bob', 'Barb'];
+friends.push('Jesse'); //add to element at the end
+console.log(friends);
+console.log(friends.length);
+
+friends.unshift('Jeffrey'); //add to beginning
+console.log(friends);
+
+const popped = friends.pop();//removes last element
+console.log(friends);
+console.log(popped);
+
+friends.shift();//removed first element
+console.log(friends);
+
+console.log(friends.indexOf('David'));//shows 0
+console.log(friends.indexOf('Heather')); //shows -1, because not in the array
+
+console.log(friends.includes('David'));//shows true
+console.log(friends.includes('John'));//shows false
+
+if(friends.includes('David')) console.log("You have a friend called David");
+*/
+
+//Coding challenge
+// const calcTip = bill => (bill >= 50 && bill <=300) ? bill * .15 : bill * .20;
+//also write as 
+
+// const calcTip = function(bill){
+//     return bill >=50 && bill <= 300 ? bill * .15 : bill * .20;
+// }
+
+function calcTip (bill){
+    return bill >=50 && bill <= 300 ? bill * .15 : bill * .20;
+}
+
+const bills = [125, 500, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [calcTip(bills[0]) + bills[0], calcTip(bills[1]) + bills[1], calcTip(bills[2]) + bills[2]];
+
+console.log(tips);
+console.log(total);
+
