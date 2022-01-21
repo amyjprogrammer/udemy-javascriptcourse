3c142e7a50dd13d1a0c4db70e8569dee863439e3
amyjprogrammer <amyjprogrammer@gmail.com>
2022-01-21 18:08:14 -0500

    setting up prettier

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+//setting up prettier
+const x = 23;
+if (x === 23) console.log(23); //auto formats on save
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log(calcAge(1995));
+
+console.log(); //just do cl and enter for this now
