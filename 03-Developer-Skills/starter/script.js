0a6426688c7840febdf257f4b3df50b0cdcc7af6
amyjprogrammer <amyjprogrammer@gmail.com>
2022-01-23 23:57:10 -0500

    debugging

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,12 +1,42 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
 //setting up prettier
 const x = 23;
 if (x === 23) console.log(23); //auto formats on save
 
 const calcAge = birthYear => 2037 - birthYear;
 
-console.log(calcAge(1995));
+console.log(calcAge(1994));
 
 console.log(); //just do cl and enter for this now
+*/
+
+//Debugging
+
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'celsius',
+//     value: prompt('Degrees celsuis:'),
+//   };
+
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+// console.log(measureKelvin());// this shows the wrong number...line 25 is wrong
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: prompt('Degrees celsuis:'),
+  };
+
+  const kelvin = parseFloat(measurement.value) + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
